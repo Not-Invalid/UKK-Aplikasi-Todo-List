5a4488ace09a298de5fc2b15a1f716925fddfc0a
RaffDev31 <raffli4000@hotmail.com>
2025-04-07 09:01:18 +0700

    refactor: remove sweetAlert2 import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,1 @@
 import './bootstrap';
-
-import Swal from 'sweetalert2';
-window.Swal = Swal;
-
-// import toastr from 'toastr';
-// import 'toastr/build/toastr.min.css';
-// window.toastr = toastr;
