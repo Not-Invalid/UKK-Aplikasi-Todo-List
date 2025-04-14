12f3fd868a41f8b20cbf9a8328c3bcbc938a236e
RaffDev31 <raffli4000@hotmail.com>
2025-04-14 11:13:45 +0700

    refactor(routes): replace controller-based routes with closure-based routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', function() {
+    return view('dashboard');
+})->name('dashboard');
 
+Route::get('tasks', function() {
+    return view('tasks.index');
+})->name('tasks');
 
-Route::get('tasks', [TasksController::class, 'index'])->name('tasks');
-Route::get('task/create', [TasksController::class, 'create'])->name('tasks.create');
-Route::get('task/1/edit',[TasksController::class, 'edit'])->name('tasks.edit');
+Route::get('tasks/create', function() {
+    return view('tasks.create');
+})->name('tasks.create');
+
+Route::get('tasks/1/edit', function() {
+    return view('tasks.edit');
+})->name('tasks.edit');
+
